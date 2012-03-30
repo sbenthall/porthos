d466899e1e6b6fc10dea7998713c06e206168d0d
Alex Kantchelian <alex.kantchelian@gmail.com>
2012-03-29 18:12:56 -0700

    create input files for mallet

---
+++ extract.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+import cPickle
+import sys
+
+def main():
+    prefix="."
+    if len(sys.argv)>1:
+        prefix=sys.argv[1]
+    prefix=prefix+"/"
+    print("Extracting in", prefix)
+    db=cPickle.load(open('data-all.db', 'r'))
+    for key in db:
+        if db[key]['lang']=='en':
+            print(db[key]['clean'].encode('utf8'), 
+                  file=open(prefix+str(db[key]['label'])+" "+key+".txt", "w"))
+    exit(0)
+
+if __name__ == "__main__":
+    main()
