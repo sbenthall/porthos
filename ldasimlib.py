a08483294cc9654d9f8f0c49f42f42e12017f293
Sebastian Benthall <sbenthall@gmail.com>
2012-05-06 19:55:28 -0700

    more work on similarity measures and refactoring

---
+++ ldasimlib.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+# <nbformat>2</nbformat>
+
+# <codecell>
+
+import cPickle
+import gensim
+import re
+import text_processing
+reload(text_processing)
+stopwords=text_processing.stopwords
+from text_processing import to_bow
+from zlib import decompress
+from numpy import random, mean, save
+from math import sqrt
+from pylab import hist, legend, title, xlabel, ylabel, show, savefig, clf
+
+
+#set of interesting htmls to consider
+def candidates(html,w2i):
+    candidates = set()
+    
+    for key in html:
+        for comp in html[key]:
+            bag=to_bow(decompress(comp), w2i)
+            if len(bag)>0:
+                if key not in candidates:
+                    candidates.add(key)
+
+    return candidates
+
+def to_nbow(text,w2i):
+    return [[w2i[x[0]], x[1]] for x in to_bow(text, w2i).items() if x[0] not in stopwords]
+
+def build_corpus(pcandidates,w2i):
+    corpus=[]
+    for key in pcandidates[:10000]:
+        for comp in html[key]:
+            corpus.append(to_nbow(decompress(comp)),w2i)
+    return corpus
+
+#compute cosine similarity of two vectors x and y
+def cosine(x, y):
+    Nx=0
+    Ny=0
+    num=0
+    j=0
+    i=0
+    while i < len(x):
+        if j < len(y):
+            if x[i][0]==y[j][0]:
+                num += x[i][1]*y[j][1]
+                Nx+=x[i][1]**2
+                Ny+=y[j][1]**2
+                i+=1
+                j+=1
+                continue
+            if x[i][0]<y[j][0]:
+                Nx+=x[i][1]**2
+                i+=1
+                continue
+            if x[i][0]>y[j][0]:
+                Ny+=y[j][1]**2
+                j+=1
+                continue
+        else:
+            Nx+=x[i][1]**2
+            i+=1
+    while j < len(y):
+        Ny+=y[j][1]**2
+        j+=1
+    return num/sqrt(Nx*Ny)
+
+# computes topic vector cosine similarity of
+# two bags of words, given an LDA model
+def lda_cosine(tw,ht,model):
+    d = []
+    for i in range(20):
+        twv=model[tw]
+        htv=model[ht]
+        d.append(cosine(twv, htv))
+    return mean(d)    
+
+# compute jaccard similarity between two bags of words
+def jaccard(tw,ht,model='dummy'):
+    tw_keys = set([p[0] for p in tw])
+    ht_keys = set([p[0] for p in ht])    
+    return float(len(tw_keys & ht_keys)) / len(tw_keys | ht_keys)
+
+# compute TF-IDF cosine similarity between two bags of words
+def tfidfcosine(tw,ht,tfidfmodel):
+    import pdb; pdb.set_trace()
+    return cosine(tfidfmodel[tw],tfidfmodel[ht])
+
+# returns a dictionary with keys from the 
+# tweets dictionary and values that are
+# tuples with tweet and HTML bags of words
+#
+# keys are ignored if tweets or HTML do
+# not contain useful information
+def filter_tweets(tweets,html,w2i,cutoff=1000):
+    good_tweet_bags = {}
+    k = 0
+    for key in tweets:
+        k+=1
+        if k > cutoff:
+            break
+        if tweets[key]['lang_infered']=='en':
+            if key in html:
+                tw=to_nbow(tweets[key]['text_clean'],w2i)
+                if len(tw)>= 1 and len(html[key])>0: 
+                    ht=to_nbow(decompress(html[key][0]),w2i)
+                    if len(ht)>=1:
+                        good_tweet_bags[key] = (tw,ht)
+    return good_tweet_bags
+
+def ham_spam_similarities(tweet_bags,bow_sim,tweets,model='dummy'):
+    k=0
+    spam=[]
+    ham=[]
+    for key in tweet_bags:
+        tw, ht = tweet_bags[key]
+        sim = bow_sim(tw,ht,model)
+        if tweets[key]['label']==1:
+            spam.append(sim)
+        else:
+            ham.append(sim)
+    return ham,spam
+
+
+def make_cdf(ham,spam,similarity_name):
+    hist(ham, alpha=0.5, bins=100, normed=True, cumulative=True, histtype='stepfilled', label='Ham ('+str(len(ham))+')', color='b')
+    hist(spam, alpha=0.5, bins=100, normed=True, cumulative=True, histtype='stepfilled', label='Spam ('+str(len(spam))+')', color='r')
+    legend(loc=2)
+    title("CDF of %s similarity for spam and ham" % (similarity_name))
+    xlabel("%s similarity" % (similarity_name))
+    ylabel("proportion")
+    savefig("%s_cdf.png" % similarity_name)
+    show()
+    clf()
+
+'''
+for key in tweets:
+    if tweets[key]['lang_infered']=='en':
+        if tweets[key]['label']==0:
+            print(tweets[key]['text_original'])
+
+
+save('gammas-spam-ham', full_matrix)
+save('labels', labels)
+
+
+[i2w[x[0]] for x in corpus[0]]
+
+model_all.show_topics(25)
+
+len(stopwords)
+
+stopwords
+'''
---
+++ ldasimscript.py
@@ -0,0 +1,62 @@
+from ldasimlib import *
+import cPickle
+import gensim
+from zlib import decompress
+
+
+HTML_DATA = 'html.db'
+
+print 'Loading data'
+
+i2w, w2i = cPickle.load(open('i2w-w2i.db','r'))
+html=cPickle.load(open('html.db', 'r'))
+
+#print 'Computing candidates'
+#candidates = candidates(html,w2i)
+
+#print 'Permuting candidates'
+#random.seed(0)
+#pcandidates=random.permutation(list(candidates))
+
+#print 'Building corpus'
+
+#corpus = build_corpus(pcandidates)
+#print 'Saving corpus'
+#cPickle.dump(corpus,open('corpus.pkl','w'))
+
+print 'Loading corpus'
+corpus = cPickle.load(open('corpus.pkl','r'))
+
+#print 'Generating model'
+#model=gensim.models.ldamodel.LdaModel(corpus, id2word=i2w, num_topics=100)
+
+#print 'Saving model'
+#model.save('model.pkl')
+
+print 'Loading LDA model'
+model = gensim.models.ldamodel.LdaModel.load('model.pkl')
+
+print 'Generating TF-IDF model'
+tfidfmodel = gensim.models.tfidfmodel.TfidfModel(corpus)
+
+print 'Loadinging tweets'
+tweets=cPickle.load(open('tweets+.db', 'r'))
+
+print 'Filtering tweets'
+            
+tweet_bags = filter_tweets(tweets,html,w2i,cutoff=2000)
+cPickle.dump(tweet_bags,open('tweet-bags.pkl','w'))
+
+print 'Computing similarities with LDA Cosine'
+ham,spam = ham_spam_similarities(tweet_bags,lda_cosine,tweets,model)
+make_cdf(ham,spam, 'lda cosine')
+
+print 'Computing similarities with Jaccard'
+ham,spam = ham_spam_similarities(tweet_bags,jaccard,tweets)
+make_cdf(ham,spam, 'Jaccard')
+
+print 'Computing similarities with TFIDF Cosine'
+ham,spam = ham_spam_similarities(tweet_bags,tfidfcosine,tweets,tfidfmodel)
+make_cdf(ham,spam, 'tf-idf cosine')
+
+
